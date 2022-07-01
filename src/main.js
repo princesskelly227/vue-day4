13f6a5bcf588561d7b8ed84e121937ca1f859ce3
lxh15 <1392649188@qq.com>
2022-07-01 18:16:15 +0800

    完成了格式化时间

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 默认找文件夹下的index文件(但是这个不是所以需要写路径)
+import "bootstrap/dist/css/bootstrap.css"
 new Vue({
   render: h => h(App),
 }).$mount('#app')
